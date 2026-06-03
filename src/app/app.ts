3409f5227ad1123081c58d0d3b53c224e62cebf2
paulo-dutraa83 <paulo.dutraa83@gmail.com>
2026-06-03 13:15:50 -0300

    versão inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-produtos');
+
+  //Criando um objeto da biblioteca HttpClient
+  httpClient = inject(HttpClient);
+
+  //Atributo para capturar o nome do produto
+  nomeProduto: string = '';
+
+  //Função que será executada quando o usuário clicar no botão de pesquisa 
+  pesquisarProdutos() {
+    //Fazendo uma requisição GET para consultar os produtos na API
+    this.httpClient
+      .get('http://localhost:8081/api/v1/produtos/listar?nome=' + this.nomeProduto)
+      .subscribe((data) => { //Capturando a resposta da API
+        console.log(data); //Exibindo a resposta no console do navegador
+      });
+  }
 }
