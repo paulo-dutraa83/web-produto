7b536c7dfb9328347f124496e612999768a40cfa
paulo-dutraa83 <paulo.dutraa83@gmail.com>
2026-06-10 12:56:53 -0300

    Desencolvimento da consulta para produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   imports: [
-    FormsModule,
+    FormsModule, CommonModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
@@ -15,6 +16,9 @@ export class App {
   //Criando um objeto da biblioteca HttpClient
   httpClient = inject(HttpClient);
 
+  //Variavel Signal para armazenar a lista de produtos obtida da API
+  produtos = signal<any[]>([]);
+
   //Atributo para capturar o nome do produto
   nomeProduto: string = '';
 
@@ -22,9 +26,9 @@ export class App {
   pesquisarProdutos() {
     //Fazendo uma requisição GET para consultar os produtos na API
     this.httpClient
-      .get('http://localhost:8081/api/v1/produtos/listar?nome=' + this.nomeProduto)
+      .get<any[]>('http://localhost:8081/api/v1/produtos/listar?nome=' + this.nomeProduto)
       .subscribe((data) => { //Capturando a resposta da API
-        console.log(data); //Exibindo a resposta no console do navegador
+        this.produtos.set(data); //Armazenar os dados obtidos da API no signal
       });
   }
 }
